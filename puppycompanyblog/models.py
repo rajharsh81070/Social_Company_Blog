8f8fbb03d7639bc1e161a4c3b9ba901d680eeede
rajharsh81070 <rajharsh81070@gmail.com>
2019-07-31 02:06:53 +0530

    Added app.py

---
+++ puppycompanyblog/models.py
@@ -0,0 +1,52 @@
+from puppycompanyblog import db,login_manager
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+
+    # Create a table in the db
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    profile_image = db.Column(db.String(20), nullable=False, default='default_profile.png')
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    # This connects BlogPosts to a User Author.
+    posts = db.relationship('BlogPost', backref='author', lazy=True)
+
+    def __init__(self, email, Username, password):
+        self.email = email
+        self.username = Username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f"UserName: {self.username}"
+
+class BlogPost(db.Model):
+    # Setup the relationship to the User table
+    users = db.relationship(User)
+    # Model for the Blog Posts on Website
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(140), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    def __init__(self, title, text, user_id):
+        self.title = title
+        self.text = text
+        self.user_id =user_id
+
+
+    def __repr__(self):
+        return f"Post Id: {self.id} --- Date: {self.date} --- Title: {self.title}"
